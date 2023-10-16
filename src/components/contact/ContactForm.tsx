93c42241573ca8c61a1ff8f33d2cde6e55f2dbc8
aynpat <aynpat21@gmail.com>
2023-10-16 19:10:34 +0800

    add reset to forms

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,13 +1,21 @@
-import  { useRef, FormEvent } from 'react';
+import  { useRef, FormEvent,useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { FormContainer } from './Contact.styled';
 import { StyleButton } from '../styles/Header.style';
 
 export const ContactForm = () => {
+
+  const [ nameVal ,setNameVal ] = useState<string>('')
+  const [ emailVal ,setEmailVal ] = useState<string>('')
+  const [ messageVal ,setMessageVal ] = useState<string>('')
+
   const form = useRef<HTMLFormElement | null>(null);
 
   const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+      setNameVal('')
+      setEmailVal('')
+      setMessageVal('')
 
     if (form.current) {
       emailjs.sendForm('service_lcc0pg9', 'template_3amweqa', form.current, 'v10rsF42vEO4HbXtS')
@@ -24,11 +32,11 @@ export const ContactForm = () => {
     <FormContainer>
         <form ref={form} onSubmit={sendEmail}>
         
-        <input type="text" name="user_name" placeholder='Name'/>
+        <input type="text" name="user_name" onChange={(e) => setNameVal(e.target.value)} value={nameVal} placeholder='Name hwllo test'/>
        
-        <input type="email" name="user_email" placeholder='Email' />
+        <input type="email" name="user_email" onChange={(e) => setEmailVal(e.target.value)} value={emailVal} placeholder='Email' />
         
-        <textarea name="message" rows={11} cols={40} placeholder='Send me a message...' />
+        <textarea name="message" rows={11} cols={40} onChange={(e) => setMessageVal(e.target.value)} value={messageVal} placeholder='Send me a message...' />
       
         <StyleButton as='button' type='submit' value='send'>SEND MESSAGE</StyleButton>
         </form>
